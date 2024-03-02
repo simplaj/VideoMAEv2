7d659c92a133d088c9d23fdceb98f81c6ff3492d
tzh <zhihuatean@gmail.com>
2024-03-02 16:22:34 +0800

    add three class stats

--- vote.py
+++ vote.py
@@ -1,4 +1,7 @@
 import os
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from stats import *
 
 
 def load_data(path):
@@ -7,7 +10,7 @@ def load_data(path):
         lines = file.readlines()[1:]
     for line in lines:
         id_log_target = line.replace(',', '').replace('[', '').replace(']', '').split()
-        idx = id_log_target[0][:-2]
+        idx = id_log_target[0]
         logits = list(map(float, id_log_target[1:4]))
         labels = id_log_target[4]
         preds = logits.index(max(logits))
@@ -15,14 +18,27 @@ def load_data(path):
             res[idx]['preds'].append(preds)
         else:
             res[idx] = {
-                'preds': [preds],
-                'labels': labels
+                'logits': logits,
+                'preds': [int(preds)],
+                'labels': int(labels),
             }
-        print(idx, logits, preds, labels)
-    print(res)
+    #     print(idx, logits, preds, labels)
+    # print(res)
+    return res
         
-        
+
+def cal_CM(res):
+    pred_label = np.array([[res[x]['preds'][0], res[x]['labels']] for x in res])
+    preds = pred_label[:, 0]
+    labels = pred_label[:, 1]
+    cm = confusion_matrix(labels, preds)
+    # print(cm)
+    draw_cm(labels, preds)
+    draw_score(labels, preds)
+    drawdis(labels, preds)
+    get_acc(labels, preds)
         
 
 if __name__ == '__main__':
-    load_data('test_results/pd_finetune/0.txt')
+    res = load_data('test_results/vit_b_pd_ft/0.txt')
+    cal_CM(res)
