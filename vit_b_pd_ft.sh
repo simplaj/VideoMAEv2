6cd52030072610501538962d125faf2365512d62
tzh <zhihuatean@gmail.com>
2024-03-02 11:13:49 +0800

    add videomaev2 base finutune

---
+++ vit_b_pd_ft.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+OMP_NUM_THREADS=1
+
+OUTPUT_DIR='train_results/vit_b_pd_ft'
+DATA_PATH='pd_data'
+MODEL_PATH='vit_b_k710_dl_from_giant.pth'
+
+python run_class_finetuning.py \
+        --model vit_base_patch16_224 \
+        --data_set PD \
+        --nb_classes 3 \
+        --data_path ${DATA_PATH} \
+        --finetune ${MODEL_PATH} \
+        --log_dir ${OUTPUT_DIR} \
+        --output_dir ${OUTPUT_DIR} \
+        --batch_size 2 \
+        --input_size 224 \
+        --short_side_size 224 \
+        --save_ckpt_freq 10 \
+        --num_frames 16 \
+        --sampling_rate 4 \
+        --num_sample 2 \
+        --num_workers 10 \
+        --opt adamw \
+        --lr 7e-4 \
+        --drop_path 0.1 \
+        --layer_decay 0.75 \
+        --opt_betas 0.9 0.999 \
+        --weight_decay 0.05 \
+        --warmup_epochs 5 \
+        --epochs 20 \
+        --test_num_segment 2 \
+        --test_num_crop 3 \
