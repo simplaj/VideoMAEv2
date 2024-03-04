ae61de9fc3ac1eb300f5d4af511a8b741de29126
tzh <zhihuatean@gmail.com>
2024-03-04 19:50:06 +0800

    test another weight

--- vit_b_pd_ft.sh
+++ vit_b_pd_ft.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 OMP_NUM_THREADS=1
 
-OUTPUT_DIR='/root/autodl-tmp/train_results/v2/vit_b_pd_ft_weight_lr_240304'
-# OUTPUT_DIR='test_results/vit_b_pd_ft_weights'
+# OUTPUT_DIR='/root/autodl-tmp/train_results/v2/vit_b_pd_ft_weight_7e-4_lr_240304'
+OUTPUT_DIR='test_results/vit_b_pd_ft_weight_7e-4_lr_240304_19e'
 DATA_PATH='pd_data'
-MODEL_PATH='vit_b_k710_dl_from_giant.pth'
-# MODEL_PATH='/root/autodl-tmp/v2/train_results/vit_b_pd_ft_weights/checkpoint-best.pth'
+# MODEL_PATH='vit_b_k710_dl_from_giant.pth'
+MODEL_PATH='/root/autodl-tmp/train_results/v2/vit_b_pd_ft_weight_7e-4_lr_240304/checkpoint-19.pth'
 # MODEL_PATH='train_results/vit_b_pd_ft_weights/checkpoint-19.pth'
 
 python run_class_finetuning.py \
@@ -25,7 +25,7 @@ python run_class_finetuning.py \
         --num_sample 2 \
         --num_workers 10 \
         --opt adamw \
-        --lr 2e-3 \
+        --lr 7e-4 \
         --drop_path 0.1 \
         --layer_decay 0.75 \
         --opt_betas 0.9 0.999 \
@@ -37,4 +37,4 @@ python run_class_finetuning.py \
         --mixup 0 \
         --cutmix 0 \
         --smoothing 0 \
-        # --validation
+        --eval
