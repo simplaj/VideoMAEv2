01f86fee70d2c53d073afd7f9aaa56bd403c11f2
tzh <zhihuatean@gmail.com>
2024-03-04 10:58:54 +0800

    change lr

--- vit_b_pd_ft.sh
+++ vit_b_pd_ft.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 OMP_NUM_THREADS=1
 
-OUTPUT_DIR='/root/autodl-tmp/train_results/vit_b_pd_ft_weight_lr_240302'
+OUTPUT_DIR='/root/autodl-tmp/train_results/v2/vit_b_pd_ft_weight_lr_240304'
+# OUTPUT_DIR='test_results/vit_b_pd_ft_weight_lr_240302_19e'
 DATA_PATH='pd_data'
 MODEL_PATH='vit_b_k710_dl_from_giant.pth'
-# MODEL_PATH='train_results/vit_b_pd_ft/checkpoint-best.pth'
+# MODEL_PATH='/root/autodl-tmp/v2/train_results/vit_b_pd_ft_weight_lr_240302/checkpoint-19.pth'
 # MODEL_PATH='train_results/vit_b_pd_ft_weights/checkpoint-19.pth'
 
 python run_class_finetuning.py \
@@ -24,7 +25,7 @@ python run_class_finetuning.py \
         --num_sample 2 \
         --num_workers 10 \
         --opt adamw \
-        --lr 3.75e-4 \
+        --lr 2e-3 \
         --drop_path 0.1 \
         --layer_decay 0.75 \
         --opt_betas 0.9 0.999 \
